143f07cbbfe20c2fa66def58f0df25e556649a3a
Hugo Drumond <hugo.drumond@fe.up.pt>
2015-09-25 00:28:31 +0100

    Hugo e João: Parse of frustum, translate and rotate working. I add to change for sth in array because it would get the length property of the array. Replaced it by normal for.

--- src/SceneGraph.js
+++ src/SceneGraph.js
@@ -67,12 +67,16 @@ SceneGraph.prototype.parseInitials = function(rootElement) {
 
   this.initials = {};
 
-  var errors = parseInitialsFrustum(initials.getElementsByTagName('frustum'));
-  console.log(this.initials);
-  parseInitialsTranslate(initials.getElementsByTagName('translate'));
-  parseInitialsRotation(initials.getElementsByTagName('rotation'));
-  parseInitialsScale(initials.getElementsByTagName('scale'));
-  parseInitialsReference(initials.getElementsByTagName('reference'));
+  var errors = this.parseInitialsFrustum(initials.getElementsByTagName('frustum'));
+  if (errors !== undefined) return errors;
+  errors = this.parseInitialsTranslate(initials.getElementsByTagName('translate'));
+  if (errors !== undefined) return errors;
+  errors = this.parseInitialsRotation(initials.getElementsByTagName('rotation'));
+  if (errors !== undefined) return errors;
+  //errors = parseInitialsScale(initials.getElementsByTagName('scale'));
+  //if (errors !== undefined) return errors;
+  //errors = parseInitialsReference(initials.getElementsByTagName('reference'));
+  //if (errors !== undefined) return errors;
 
   // iterate over every element
   //var initialsLen = initials.children.length;
@@ -95,16 +99,51 @@ SceneGraph.prototype.onXMLError = function(message) {
   this.isLoaded = false;
 };
 
-function parseInitialsFrustum(frustumArray, initials) {
-  if (frustumArray === null || frustumArray.length !== 1) return 'There must be a frustum';
+SceneGraph.prototype.parseInitialsFrustum = function(frustumArray) {
+  if (frustumArray === null || frustumArray.length !== 1) return 'frustum must exist and only once';
 
-  initials.frustum = {};
-  initials.frustum.near = frustumArray[0].attributes.getNamedItem('near').value;
-  initials.frustum.far = frustumArray[0].attributes.getNamedItem('far').value;
+  this.initials.frustum = {};
+  this.initials.frustum.near = this.reader.getFloat(frustumArray[0], 'near');
+  if (this.initials.frustum.near === null || isNaN(this.initials.frustum.near)) {
+    return 'frustum must have a near attribute with a numeric value';
+  }
+  this.initials.frustum.far = this.reader.getFloat(frustumArray[0], 'far');
+  if (this.initials.frustum.far === null || isNaN(this.initials.frustum.far)) {
+    return 'frustum must have a far attribute with a numeric value';
+  }
 
 };
 
-//function parseInitialsTranslate();
-//function parseInitialsRotation();
+SceneGraph.prototype.parseInitialsTranslate = function(translateArray) {
+  if (translateArray === null || translateArray.length !== 1) return 'translate must exist and only once';
+
+  this.initials.translate = {};
+  var coordinates = 'xyz';
+  for (var index in coordinates) {
+    this.initials.translate[coordinates[index]] = this.reader.getFloat(translateArray[0], coordinates[index]);
+    if (this.initials.translate[coordinates[index]] === null || isNaN(this.initials.translate[coordinates[index]])) {
+      return 'translate must have an ' + coordinates[index] + ' attribute with a numeric value';
+    }
+  }
+};
+SceneGraph.prototype.parseInitialsRotation = function(rotationArray) {
+  if (rotationArray === null || rotationArray.length !== 3) return 'rotation must exist, one for each coordinate: x, y, z.';
+
+  this.initials.rotation = {};
+  var coordinates = ['x', 'y', 'z'];
+  for (var rotationIndex = 0; rotationIndex < rotationArray.length; ++rotationIndex) {
+    var coordinate = this.reader.getString(rotationArray[rotationIndex], 'axis');
+    var coordinateIndex = coordinates.findIndex(function(value) {
+      if (value === coordinate) return true;
+    });
+    if (coordinateIndex === -1) return 'The axis attribute must be either: x, y or z.';
+    delete coordinates[coordinateIndex];
+
+    this.initials.rotation[coordinate] = this.reader.getFloat(rotationArray[rotationIndex], 'angle');
+    if (this.initials.rotation[coordinate] === null || isNaN(this.initials.rotation[coordinate])) {
+      return 'The angle value must be a number.';
+    }
+  }
+};
 //function parseInitialsScale();
 //function parseInitialsReference();
--- src/main.js
+++ src/main.js
@@ -60,7 +60,7 @@ serialInclude(['../lib/CGF.js', 'XMLscene.js', 'SceneGraph.js',
     // get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
     // or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-    var filename = getUrlVars()['file'] || "lsx.xml";
+    var filename = getUrlVars()['file'] || "test_file.xml";
 
     // create and load graph, and associate it to scene.
     // Check console for loading errors
