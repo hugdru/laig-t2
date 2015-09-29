2440a6bfb9495a255847d9e234efa18f409d3404
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-09-29 17:38:04 +0100

    Added verification to the number of attributes in the INITIALS elements

--- src/LSXParseInitials.js
+++ src/LSXParseInitials.js
@@ -3,7 +3,7 @@ LSXParser.prototype.parseInitials = function(rootElement) {
 
   var initialsArray = rootElement.getElementsByTagName('INITIALS');
   if (initialsArray === null || initialsArray.length !== 1) {
-    return 'There must be one and only one INITIALS';
+    return 'There must be one and only one INITIALS.';
   }
 
   var initials = initialsArray[0];
@@ -35,29 +35,35 @@ LSXParser.prototype.parseInitials = function(rootElement) {
 
 LSXParser.prototype.parseInitialsFrustum = function(frustumArray) {
   if (frustumArray === null || frustumArray.length !== 1)
-    return 'There must be one and only one frustum in INITIALS';
+    return 'There must be one and only one frustum in INITIALS.';
 
   var frustum = frustumArray[0];
+
+  if (frustum.attributes.length !== 2)
+    return 'Frustum must have exactly two attributes: near and far.';
 
   this.graph.initials.frustum = {};
 
   this.graph.initials.frustum.near = this.reader.getFloat(frustum, 'near');
   if (this.graph.initials.frustum.near === null || isNaN(this.graph.initials.frustum.near)) {
-    return 'Frustum must have a near attribute with a numeric value';
+    return 'Frustum must have a near attribute with a numeric value.';
   }
 
   this.graph.initials.frustum.far = this.reader.getFloat(frustum, 'far');
   if (this.graph.initials.frustum.far === null || isNaN(this.graph.initials.frustum.far)) {
-    return 'Frustum must have a far attribute with a numeric value';
+    return 'Frustum must have a far attribute with a numeric value.';
   }
 
 };
 
 LSXParser.prototype.parseInitialsTranslate = function(translateArray) {
   if (translateArray === null || translateArray.length !== 1)
-    return 'There must be one and only one translate in INITIALS';
+    return 'There must be one and only one translate in INITIALS.';
 
   var translate = translateArray[0];
+
+  if (translate.attributes.length !== 3)
+    return 'Translate must have exactly three attributes: x, y, z.';
 
   this.graph.initials.translate = {};
 
@@ -67,13 +73,13 @@ LSXParser.prototype.parseInitialsTranslate = function(translateArray) {
     this.graph.initials.translate[coordinates[index]] = this.reader.getFloat(translate, coordinates[index]);
 
     if (this.graph.initials.translate[coordinates[index]] === null || isNaN(this.graph.initials.translate[coordinates[index]]))
-      return 'Translate must have an ' + coordinates[index] + ' attribute with a numeric value';
+      return 'Translate must have an ' + coordinates[index] + ' attribute with a numeric value.';
   }
 };
 
 LSXParser.prototype.parseInitialsRotation = function(rotationArray) {
   if (rotationArray === null || rotationArray.length !== 3)
-    return 'There must be one and only one rotation for each coordinate: x,y,z in INITIALS';
+    return 'There must be one and only one rotation for each coordinate: x,y and z in INITIALS.';
 
   this.graph.initials.rotation = {};
 
@@ -84,6 +90,9 @@ LSXParser.prototype.parseInitialsRotation = function(rotationArray) {
   };
 
   for (var rotationIndex = 0; rotationIndex < rotationArray.length; rotationIndex++) {
+    if (rotationArray[rotationIndex].attributes.length !== 3)
+      return 'Rotation must have exactly two attributes: axis and angle.';
+
     var coordinate = this.reader.getString(rotationArray[rotationIndex], 'axis');
 
     var coordinateIndex = coordinates.findIndex(isACoordinate);
@@ -96,15 +105,18 @@ LSXParser.prototype.parseInitialsRotation = function(rotationArray) {
     this.graph.initials.rotation[coordinate] = this.reader.getFloat(rotationArray[rotationIndex], 'angle');
 
     if (this.graph.initials.rotation[coordinate] === null || isNaN(this.graph.initials.rotation[coordinate]))
-      return 'Rotation must have an angle attribute with a numeric value';
+      return 'Rotation must have an angle attribute with a numeric value.';
   }
 };
 
 LSXParser.prototype.parseInitialsScale = function(scaleArray) {
   if (scaleArray === null || scaleArray.length !== 1)
-    return 'There must be one and only one translate in INITIALS';
+    return 'There must be one and only one translate in INITIALS.';
 
   var scale = scaleArray[0];
+
+  if (scale.attributes.length !== 3)
+    return 'Scale must have exactly three attributes: sx, sy, sz.';
 
   this.graph.initials.scale = {};
 
@@ -114,18 +126,21 @@ LSXParser.prototype.parseInitialsScale = function(scaleArray) {
     this.graph.initials.scale[coordinates[index]] = this.reader.getFloat(scale, 's' + coordinates[index]);
 
     if (this.graph.initials.scale[coordinates[index]] === null || isNaN(this.graph.initials.scale[coordinates[index]]))
-      return 'Scale must have a ' + coordinates[index] + ' attribute with a numeric value';
+      return 'Scale must have a s' + coordinates[index] + ' attribute with a numeric value.';
   }
 };
 
 LSXParser.prototype.parseInitialsReference = function(referenceArray) {
   if (referenceArray === null || referenceArray.length !== 1)
-    return 'There must be one and only one translate in INITIALS';
+    return 'There must be one and only one translate in INITIALS.';
 
   var reference = referenceArray[0];
+
+  if (reference.attributes.length !== 1)
+    return 'Reference must have exactly one attributes: length.';
 
   this.graph.initials.reference = this.reader.getFloat(reference, 'length');
 
   if (this.graph.initials.reference === null || isNaN(this.graph.initials.reference) || this.graph.initials.reference < 0)
-    return 'Reference must have a length attribute with a numeric value >= 0';
+    return 'Reference must have a length attribute with a numeric value >= 0.';
 };
